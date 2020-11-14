9dee04d046c7a3d6203c08d36d7bcc802afdd924
RoJuan230 <rohan.nakra100@gmail.com>
2020-11-13 21:48:18 -0800

    Initial commit.

---
+++ cancer_dataset.py
@@ -0,0 +1,8 @@
+# Objective: Predict if a person is bengin or malignant for breast cancer.
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.manifold import TSNE
+from sklearn.metrics import recall_score
+
+
